877200dcba9f680b464003eed8c37abcf5e28fe8
YAOSHENG <41530023+DengYaosheng@users.noreply.github.com>
2024-08-21 16:37:48 +0800

    Create canteen_data.py

---
+++ canteen_data.py
@@ -0,0 +1,18 @@
+# canteen_data.py
+
+canteens = [
+    {"name": "Canteen 1", "location": (1.3456, 103.6789)},
+    {"name": "Canteen 2", "location": (1.3467, 103.6790)},
+    {"name": "Canteen 3", "location": (1.3478, 103.6791)},
+    {"name": "Canteen 4", "location": (1.3489, 103.6792)},
+    {"name": "Canteen 5", "location": (1.3490, 103.6793)},
+    {"name": "Canteen 6", "location": (1.3501, 103.6794)},
+    {"name": "Canteen 7", "location": (1.3512, 103.6795)},
+    {"name": "Canteen 8", "location": (1.3523, 103.6796)},
+]
+
+def get_canteen_data():
+    """
+    返回食堂的GPS位置信息。
+    """
+    return canteens
